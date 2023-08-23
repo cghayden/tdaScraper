63e60300420d728629022d00f7bb21654f7fca29
Corey Hayden <cghayden@gmail.com>
2023-08-22 21:04:05 -0400

    pared down for render cron trial

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import 'dotenv/config'
 import { scrapeTDA } from './scraper.js'
-import cron from 'node-cron'
 
-cron.schedule('*/10 * * * *', () => {
-  console.log(`running scraper at ${Date.now()}`)
-  scrapeTDA()
-})
+scrapeTDA()
