0c702d5b0421655623844725dc0521071f206b10
Corey Hayden <cghayden@gmail.com>
2023-11-06 19:51:26 -0500

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import cron from 'node-cron'
 import { scrapeTDA } from './scraper.js'
 
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('*/3 * * * *', () => {
   console.log(`running tda scraper at ${Date.now()}`)
   scrapeTDA()
 })
